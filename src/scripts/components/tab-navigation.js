e3c414f8007046bc4f68868dd2cd81291f412e7b
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2022-10-26 18:27:26 +0700

    fix responsive design

--- src/scripts/components/tab-navigation.js
+++ src/scripts/components/tab-navigation.js
@@ -14,14 +14,14 @@ class TabNavigation extends HTMLElement {
     this.innerHTML = `
     <nav class="bg-black text-white pt-7">
         <div class="flex justify-center items-center mx-auto container">
-            <ul class="nav flex flex-wrap justify-center items-center bg-slate-600 w-max lg:py-4  px-4 w-max-[400px]:px-[2px] py-4 rounded-md">
-                <li class="nav-item lg:mr-2 mr-1 w-max-[400px]:mr-1">
-                    <a id="nowPlaying" class="nav-link active bg-slate-800 cursor-pointer w-full active:bg-slate-800 hover:bg-slate-800 lg:text-xl w-max-{400px]:text-sm text-lg font-semibold  sm:px-2 px-2 py-3 lg:px-3 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Now Playing</a>
+            <ul class="nav flex flex-wrap justify-center items-center bg-slate-600 w-max lg:py-4 px-4  py-4 rounded-md">
+                <li class="nav-item lg:mr-2 sm:mx-3 mx-0 py-2">
+                    <a id="nowPlaying" class="nav-link active bg-slate-800 cursor-pointer  active:bg-slate-800 hover:bg-slate-800 lg:text-2xl sm:text-xl text-lg font-semibold  sm:px-2 px-2 py-3 lg:px-3 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Now Playing</a>
                 </li>
-                <li class="nav-item lg:mx-2 sm:mx-3 w-max-[400px]:mr-1"><a id="upcoming" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-xl w-max-{400px]:text-sm text-lg font-semibold px-2 py-3 lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Upcoming</a></li>
-                <li class="nav-item lg:mx-2"><a id="popular" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-xl w-max-{400px]:text-sm text-lg font-semibold px-3 py-3 lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Popular</a></li>
-                <li class="nav-item lg:mx-2 mx-0 mt-6 sm:mt-0"><a id="trending" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-xlw-max-{400px]:text-sm text-lg font-semibold px-2 py-3  lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Trending</a></li>
-                <li class="nav-item lg:mx-2 mx-0 mt-6 sm:mt-0"><a id="toprated" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-xlw-max-{400px]:text-sm text-lg font-semibold px-2 py-3  lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Top Rated</a></li>
+                <li class="nav-item lg:mx-2 sm:mx-3 mx-2"><a id="upcoming" class="nav-link cursor-pointer active:bg-slate-800 sm:text-xl hover:bg-slate-800 lg:text-2xl text-lg  font-semibold px-2 py-3 lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Upcoming</a></li>
+                <li class="nav-item lg:mx-2 sm:mx-3"><a id="popular" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-2xl text-lg font-semibold px-3 py-3 sm:text-xl lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Popular</a></li>
+                <li class="nav-item lg:mx-2 mx-2 mt-0 max-[400px]:mt-6 sm:mt-0 sm:mx-3 md:mt-0"><a id="trending" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-2xl sm:text-xl text-lg font-semibold px-2 py-3  lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Trending</a></li>
+                <li class="nav-item lg:mx-2 mx-0 mt-6 lg:mt-0 sm:mt-5 md:mt-0 sm:mx-3"><a id="toprated" class="nav-link cursor-pointer active:bg-slate-800 hover:bg-slate-800 lg:text-2xl sm:text-xl text-lg font-semibold px-2 py-3  lg:px-5 lg:py-3 rounded-lg ease-in-out transition-all duration-100">Top Rated</a></li>
             </ul>
         </div>
     </nav>`;
